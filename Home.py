60918b9d716c082f423c000dc7c2ac659e4e8cf4
firmanh3200 <135565052+firmanh3200@users.noreply.github.com>
2024-11-25 11:35:34 +0700

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+st.title(':orange[STATISTIK] :blue[DAERAH] :green[KOTA BANJAR]')
+st.subheader('', divider='rainbow')
+st.success('Aplikasi ini berisi kumpulan Indikator Makro yang resmi dirilis oleh Badan Pusat Statistik, \
+    ditambah data-data statistik sektoral yang sumbernya tercantum.')
+st.info('Aplikasi ini dibuat untuk memudahkan Para Pemangku Kepentingan dalam proses Perencanaan, Pelaksanaan, \
+    Monitoring dan Evaluasi Pembangunan di Kota Banjar.')
+st.warning('Silakan mengakses setiap indikator makro melalui menu di sebelah kiri.')
+
+st.subheader('', divider='rainbow')
+st.caption(':orange[Statistik Daerah Kota Banjar]')
+st.caption('Hak Cipta @ :green[Badan Pusat Statistik Kota Banjar]')
